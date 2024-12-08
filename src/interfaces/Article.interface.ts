36fb53d91287dc2ab6020a34e91655b64a79e7ed
FrankJimenezDev <frankjimenezp113@gmail.com>
2024-12-08 00:15:07 -0300

    [Add] Se crean interfaces para Article y NavBarItems

---
+++ src/interfaces/Article.interface.ts
@@ -0,0 +1,9 @@
+export interface Article {
+    title: string;
+    text: string;
+    image: string;
+}
+
+export interface ArticlesI {
+    articles : Array<Article>
+}
---
+++ src/interfaces/NavBarItems.interface.ts
@@ -0,0 +1,4 @@
+export interface NavBarItemI {
+    Title: string;
+    path: string;
+}
