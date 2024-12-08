cb2642eda353b288983e922aecbec7777d79e6c5
FrankJimenezDev <frankjimenezp113@gmail.com>
2024-12-08 00:21:44 -0300

    [Add] Se crea el componente para los articulos de la HomePage

---
+++ src/components/ArticleComponents/Articles.tsx
@@ -0,0 +1,35 @@
+import React from "react"
+import { ArticlesI } from "../../interfaces/Article.interface"
+
+export const Articles: React.FC<ArticlesI> = ({ articles }) => {
+
+    return (
+        <section className="space-y-12">
+            {
+                articles.map((article, idx) => {
+
+                    const articleClassName = (idx % 2 === 0)
+                        ? `flex flex-col md:flex-row items-center md:items-start md:justify-center`
+                        : `flex flex-col md:flex-row-reverse items-center md:items-start md:justify-center`
+
+                    return (
+                        <article key={idx} className={articleClassName}>
+                            <div className="md:w-5/12">
+                                <h2 className="text-2xl font-bold mb-4">{article.title}</h2>
+                                <p className="text-gray-600 mb-6 text-center text-justify m-5">
+                                    {article.text}
+                                </p>
+                            </div>
+                            <figure className="md:w-5/12 max-w-sm">
+                                <img
+                                    className="rounded-md border-none"
+                                    src={article.image}
+                                    alt="Verse Finder feature illustration" />
+                            </figure>
+                        </article>
+                    )
+                })
+            }
+        </section>
+    )
+}
